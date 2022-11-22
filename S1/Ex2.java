b4d12cc324279380a28710d43ac6e10d25e9d783
Yury Titov <yuriy-t87@yandex.ru>
2022-11-22 11:22:28 +0300

    second

---
+++ S1/Ex2.java
@@ -0,0 +1,29 @@
+package HW.S1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex2 {
+    
+    private Boolean isPrime(int num)
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+
+    public List<Integer> getPrimes(int n){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(isPrime(i))
+                list.add(i);
+        }
+        return list;
+    }
+
+
+}
--- S1/Main.java
+++ S1/Main.java
@@ -1,20 +1,32 @@
 package HW.S1;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public  class Main {
     public static void main(String[] args) {
         // Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
         firstEx();
-
-
+        // Вывести все простые числа от 1 до 1000
+        SecondEx();
     }
 
 
+    private static void SecondEx(){
+        Ex2 ex2 = new Ex2();
+        List<Integer> lst = ex2.getPrimes(1000);
+        System.out.println("Вывести все простые числа от 1 до 1000");
+        System.out.println(Arrays.toString(lst.toArray()));
+        System.out.println();
+    }
+
     private static void firstEx(){
+        System.out.println("Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)");
         int num = readDataAsInt("Введите число: ");
         Ex1 ex1 = new Ex1();
         System.out.println("N-ое треугольного числа равно: " + ex1.sumTrianlgeNum(num));
         System.out.println("Факториал числа равен: " + ex1.factorial(num));
+        System.out.println();
     }
 
     private static int readDataAsInt(String msg){
